2c8c548ca1e44c242a5a929b5ed0ef6c502d805f
Advith Chelikani <advith.chelikani@gmail.com>
2021-09-15 21:37:47 -0700

    update ingestion lambda

--- scripts/bundle_ingestion_lambda.sh
+++ scripts/bundle_ingestion_lambda.sh
@@ -1,5 +1,7 @@
 mkdir ui-ingestion-lambda
 cp -R ingestion/ ui-ingestion-lambda/ingestion
+cp -R database/ ui-ingestion-lambda/database
+cp -R models/ ui-ingestion-lambda/models
 cp ingestion_lambda.py ui-ingestion-lambda/
 cp -R venv/lib/python3.8/site-packages/ ui-ingestion-lambda
 cd ui-ingestion-lambda
