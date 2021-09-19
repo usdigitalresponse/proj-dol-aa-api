025724b8674cb61cf7148cd4b6afb43c35557d29
Advith Chelikani <advith.chelikani@gmail.com>
2021-09-19 15:08:03 -0700

    exporting lambda

--- exporting_lambda.py
+++ exporting_lambda.py
@@ -6,9 +6,16 @@ It will update the metadata on these rows of the database.
 """
 from ingestion.csv_processor import CSVProcessor
 import json
+import boto3
 from database.db_connection import DatabaseConnection
 from clients.form.form_client import FormClient
 from utils.processing_helpers import convert_form_responses_to_claims
+from utils.exporting_helpers import claims_to_csv
+from models.claim import Claim, unpacking_func
+
+EXPORTS_BUCKET = "ui-claimant-exports"
+
+s3 = boto3.resource("s3")
 
 
 def lambda_handler(event, context):
@@ -24,10 +31,17 @@ def lambda_handler(event, context):
 
         for claim in claims:
             db_connection.update_row(claim)
+            num_entries += 1
 
-        # TODO: Export entire database as CSV and email to recipient.
-
+        # Export entire database as CSV and email to recipient.
+        claims = db_connection.fetch_all_rows(unpacking_func)
+        claims_to_csv(claims, "/tmp/claim_responses.csv")
         db_connection.close()
+
+        # Store CSV in s3.
+        s3.Bucket(EXPORTS_BUCKET).upload_file(
+            "/tmp/claim_responses.csv", "claim_responses.csv"
+        )
 
         return {
             "statusCode": 200,
